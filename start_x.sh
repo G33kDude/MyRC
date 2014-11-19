2c241671889887fbe26474efd78cc2907a5997c7
G33kDude <ptubbytaylor@gmail.com>
2014-11-19 10:59:52 -0500

    Renaming bash scripts

--- start_x.sh
+++ start_x.sh
@@ -2,3 +2,4 @@ export DISPLAY=:0
 screen -S xvfb -d -m Xvfb -screen 0 800x600x24
 screen -S flux -d -m fluxbox
 screen -S x11vnc -d -m x11vnc -nopw -forever -xkb -skip_keycodes 187,188
+#x11vnc -forever -usepw -auth ~/$1 -display $1
