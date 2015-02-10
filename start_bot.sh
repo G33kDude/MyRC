01d20bcba419d167d7bb3f4b3690ab5fcdbadda6
G33kDude <PtubbyTaylor@Gmail.com>
2015-02-10 17:30:55 -0500

    Changed start_bot.sh to start all bots

--- start_bot.sh
+++ start_bot.sh
@@ -1,3 +1,5 @@
 # !/bin/sh
 export DISPLAY=:0
 screen -S MyRC -d -m wine ./AutoHotkey.exe ./IRCBot.ahk
+screen -S ForumBot -d -m wine ./AutoHotkey.exe ./ForumBot.ahk
+screen -S progbot -d -m wine ./AutoHotkey.exe ./bot/dailyprogbot.ahk
