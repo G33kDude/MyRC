d3be643a878b808c95bae9161ee8a1011a97b95d
G33kDude <PtubbyTaylor@Gmail.com>
2015-03-03 23:55:39 -0500

    Update start_bot.sh with new bot paths

--- start_bot.sh
+++ start_bot.sh
@@ -1,5 +1,5 @@
 # !/bin/sh
 export DISPLAY=:0
-screen -S MyRC -d -m wine ./AutoHotkey.exe ./IRCBot.ahk
-screen -S ForumBot -d -m wine ./AutoHotkey.exe ./ForumBot.ahk
-screen -S progbot -d -m wine ./AutoHotkey.exe ./bot/dailyprogbot.ahk
+screen -S MyRC -d -m wine ./AutoHotkey.exe ./bots/IRCBot.ahk
+screen -S ForumBot -d -m wine ./AutoHotkey.exe ./bots/ForumBot.ahk
+screen -S progbot -d -m wine ./AutoHotkey.exe ./bots/dailyprogbot.ahk
