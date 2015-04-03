088692f64dda274cb1d7fef5fe66d9da6db6d899
G33kDude <PtubbyTaylor@Gmail.com>
2015-04-03 12:33:16 -0400

    Added short sleep before running vnc server in start_x.sh

--- start_x.sh
+++ start_x.sh
@@ -1,5 +1,6 @@
 export DISPLAY=:0
 screen -S xvfb -d -m Xvfb -screen 0 800x600x24
 screen -S flux -d -m fluxbox
+sleep 1
 screen -S x11vnc -d -m x11vnc -nopw -forever -xkb -skip_keycodes 187,188 -localhost
 #x11vnc -forever -usepw -auth ~/$1 -display $1
