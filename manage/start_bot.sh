f69f11ed538bcda6d33371725d773ec5194d5c2a
G33kDude <ptubbytaylor@gmail.com>
2015-10-15 09:04:07 -0400

    Updated bot start script to work with new repo layout

--- manage/start_bot.sh
+++ manage/start_bot.sh
@@ -1,5 +1,5 @@
 # !/bin/sh
 export DISPLAY=:0
-screen -S MyRC -d -m wine ./AutoHotkey.exe ./bots/IRCBot.ahk
-screen -S ForumBot -d -m wine ./AutoHotkey.exe ./bots/ForumBot.ahk
-screen -S progbot -d -m wine ./AutoHotkey.exe ./bots/dailyprogbot.ahk
+screen -S geekbot -d -m wine ../AutoHotkey.exe ../bots/GeekBot/GeekBot.ahk
+screen -S forumbot -d -m wine ../AutoHotkey.exe ../bots/ForumBot/ForumBot.ahk
+screen -S progbot -d -m wine ../AutoHotkey.exe ../bots/DailyProgBot/DailyProgBot.ahk
